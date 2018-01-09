1bf9534458325119d781b22b3943c4b14ce40909
fightingspider <35067078+fightingspider@users.noreply.github.com>
2018-01-09 13:29:29 +0800

    Update items.py

--- weibo/items.py
+++ weibo/items.py
@@ -11,10 +11,10 @@ import scrapy
 class WeiboItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    focus_by = scrapy.Field()  # 该微博主的粉丝粉丝
+    focus_by = scrapy.Field()  # 该微博主源自
     name = scrapy.Field() #微博主名字
     url = scrapy.Field()  #微博主url
-    fans_num = scrapy.Field() #粉丝数量，100万以上为大V，也是本次研究对象
+    fans_num = scrapy.Field() #粉丝数量，500万以上默认大V，也是本次研究对象
 
 class FailUrlItem(scrapy.Item):
     fail_url = scrapy.Field() #记录请求失败的url
